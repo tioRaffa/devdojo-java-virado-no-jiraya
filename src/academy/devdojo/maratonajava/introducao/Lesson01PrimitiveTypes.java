13e8f162af27512c63d21a54059fc8372d6cf625
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-08 09:57:46 -0300

    aula 02

---
+++ src/academy/devdojo/maratonajava/introducao/Lesson01PrimitiveTypes.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Lesson01PrimitiveTypes {
+    public static void  main(String[] args){
+        // int, float, char, boolean, double, byte, short, long
+        int idade = 10;
+        byte idadeByte = 10;
+        short idadeShort = 5;
+        boolean idadeMaiorBoolean = true;
+        boolean idadeMenorBoolean = false;
+        double salarioDouble = 2000.0;
+        float salarioFloat = 2500.0f;
+        long salarioLong = 50000;
+        System.out.println("A idade é "+idade   +" anos");
+    }
+
+}
