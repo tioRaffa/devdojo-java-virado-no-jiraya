667b6be91b3033b56ef672cf65d2f619a3a7dd8e
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-09 11:23:34 -0300

    aula estruturas condicionais

--- src/academy/devdojo/maratonajava/introducao/Lesson05EstruturasCondicionais02.java
+++ src/academy/devdojo/maratonajava/introducao/Lesson05EstruturasCondicionais02.java
@@ -1,4 +1,19 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Lesson05EstruturasCondicionais02 {
+    public static void main(String[] args) {
+        int idade = 17;
+        boolean isCategoriaInfatil = idade <= 14;
+        boolean isCategoriaJuvenil = idade >= 15 && idade <=17;
+        String categoria;
+
+        if (isCategoriaInfatil){
+            categoria = "Categoria Infantil";
+        } else if (isCategoriaJuvenil) {
+            categoria = "Categoria Juvenil";
+        } else {
+            categoria = "Categoria Adulta";
+        }
+        System.out.println(categoria);
+    }
 }
