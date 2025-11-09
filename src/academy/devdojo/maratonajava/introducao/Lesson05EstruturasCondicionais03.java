3fb2a31dc63bc501b2841237692ebd5adbe6b402
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-09 11:45:19 -0300

    aula operador ternário

---
+++ src/academy/devdojo/maratonajava/introducao/Lesson05EstruturasCondicionais03.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Lesson05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        double salario = 501;
+        String msgDoar = "Vou doar 500tão";
+        String msgNaoDoar = "Não vou doar 500tão";
+
+        // (condição) ? verdadeiro : falso;
+        String resultado = salario > 500 ? msgDoar : msgNaoDoar;
+
+    }
+}
