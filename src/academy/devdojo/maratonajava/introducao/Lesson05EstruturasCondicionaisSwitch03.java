b04831b8913245f6d553bd57a7d1d8054039255f
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-11 21:08:12 -0300

    aula switch

---
+++ src/academy/devdojo/maratonajava/introducao/Lesson05EstruturasCondicionaisSwitch03.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Lesson05EstruturasCondicionaisSwitch03 {
+    public static void main(String[] args){
+        // Imprima o dia da semana, considerando 1 como domingo
+        int dia = 5;
+        // String, int, char, byte, short, enum
+        switch (dia){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Domingo");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+        }
+    }
+}
