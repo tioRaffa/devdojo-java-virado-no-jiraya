51c65c680ee6b74a468f09b7d350356cb5eaef3e
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-12 19:46:37 -0300

    aula Estrutura de Array pt-1

---
+++ src/academy/devdojo/maratonajava/introducao/lesson07Arrays.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class lesson07Arrays {
+    public static void main(String[] args) {
+        int [] idades = new int[3];
+
+        for (int i = 0; i <=2; i++){
+            idades[i] = i + 1;
+            System.out.println(idades[i]);
+        }
+
+    }
+}
