173b62ac73b75829851fb3bdee82e9ea158be089
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-11 21:44:00 -0300

    aula Estrutura de Repetição

---
+++ src/academy/devdojo/maratonajava/introducao/Lesson06EstruturaDeRepeticao01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Lesson06EstruturaDeRepeticao01 {
+    public static void main(String[] args) {
+        // while e for
+        int count = 0;
+        //while
+        while (count<10){
+            System.out.println(++count);
+            //count++;
+        }
+        //for      V     C  I      V= Variavel, C= Condição e I= Incremento
+        for (int i=0; i<=5; i++){
+            System.out.println(i);
+        }
+
+    }
+}
