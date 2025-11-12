5974ed80ae5d58b0ef2567b68a69dfab12b8883d
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-11 22:16:54 -0300

    aula Estrutura de Repetição ex

---
+++ src/academy/devdojo/maratonajava/introducao/Lesson06EstruturaDeRepeticao02ex.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Lesson06EstruturaDeRepeticao02ex {
+    public static void main(String[] args) {
+
+        for (int i=0; i <= 100; i++){
+            boolean isPar = (i % 2 == 0);
+
+            if (isPar && i <=25){
+                System.out.println("xx "+i+" xx");
+            } else {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
