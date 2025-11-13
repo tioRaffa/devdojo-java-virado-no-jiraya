f452b711f9806895793611fd6c326df8e6df65fe
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-12 23:19:15 -0300

    aula de Introdução ao POO - COESÃO

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/dominio/TeacherClass.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.dominio;
+
+public class TeacherClass {
+    public String name;
+    public int age;
+    public char gender;
+    public String school;
+}
