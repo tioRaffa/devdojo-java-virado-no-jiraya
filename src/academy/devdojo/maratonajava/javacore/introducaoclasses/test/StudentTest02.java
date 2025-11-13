7bbe3d1399c082b6585de2a6ec6a597ece79b403
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-12 22:18:14 -0300

    aula de Introdução ao POO - CLASSES pt2

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/test/StudentTest02.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.test;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Student;
+
+public class StudentTest02 {
+    public static void main(String[] args) {
+        Student student02 = new Student();
+
+
+    }
+}
