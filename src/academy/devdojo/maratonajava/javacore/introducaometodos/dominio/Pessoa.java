752fbcd242089c9d035b484e550a653f398ff676
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-15 22:41:28 -0300

    aula de Introdução ao POO - Modificador de acesso private, get e set pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/introducaometodos/dominio/Pessoa.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.introducaometodos.dominio;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public int getIdade() {
+        return this.idade;
+    }
+}
