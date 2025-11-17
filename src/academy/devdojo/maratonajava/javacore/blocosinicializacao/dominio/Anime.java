8d47e310b5d134298b2a7a42cff3d7560a9220dc
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-16 22:32:08 -0300

    aula de Introdução ao POO - Construtores pt-1 -  Blocos de inicialização

---
+++ src/academy/devdojo/maratonajava/javacore/blocosinicializacao/dominio/Anime.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.blocosinicializacao.dominio;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+
+    // Bloco Contrutor -- Quase NUNCA usado!!
+    {
+        episodios = new int[10];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public int[] getEpisodios(){
+        return episodios;
+    }
+
+}
