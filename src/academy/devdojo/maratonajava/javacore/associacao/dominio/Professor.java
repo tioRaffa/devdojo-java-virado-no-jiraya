85623cfb5504f825d934151a8dca910ea44870b0
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-17 23:04:01 -0300

    Orientação Objetos - Associação pt 03 - Associação unidirecional muitos para um

---
+++ src/academy/devdojo/maratonajava/javacore/associacao/dominio/Professor.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.associacao.dominio;
+
+public class Professor {
+    private String nome;
+    public Professor(String nome){
+        this.nome = nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+}
