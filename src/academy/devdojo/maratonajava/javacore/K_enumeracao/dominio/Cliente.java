470ad0b5c706a498b3b5399e1e58d4b3475b63d5
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-21 20:12:40 -0300

    Orientação Objetos - Enumeração pt 01.1

---
+++ src/academy/devdojo/maratonajava/javacore/K_enumeracao/dominio/Cliente.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.K_enumeracao.dominio;
+
+public class Cliente {
+    private String nome;
+    private String tipo;
+
+    public Cliente(String nome, String tipo){
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+} 
