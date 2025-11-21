16347d41e436141debf73aae5fac182a360093f7
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-11-20 21:09:12 -0300

    Orientação Objetos - Herança pt 02 - @Iverride & Super

--- src/academy/devdojo/maratonajava/javacore/H_heranca/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/H_heranca/dominio/Funcionario.java
@@ -17,4 +17,9 @@ public class Funcionario extends Pessoa{
     public void setSalario(double salario) {
         this.salario = salario;
     }
+    @Override
+    public void imprime(){
+        super.imprime();
+        System.out.println("Salario: R$" + getSalario());
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/H_heranca/test/FuncionarioTest01.java
+++ src/academy/devdojo/maratonajava/javacore/H_heranca/test/FuncionarioTest01.java
@@ -8,6 +8,7 @@ public class FuncionarioTest01 {
         Funcionario f = new Funcionario("Rafael", 23);
         Endereco e = new Endereco("Sete de Setembro", "88403-240");
         f.setEndereco(e);
+        f.setSalario(2500);
         f.imprime();
     }
 }
